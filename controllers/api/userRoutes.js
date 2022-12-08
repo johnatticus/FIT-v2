d5508c761d57986792c1f2d245751aa81cba433a
John Hysong <johnahysong@gmail.com>
2022-12-07 23:14:24 -0500

    not much

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -65,6 +65,7 @@ router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
+      console.log(res.status)
     });
   } else {
     res.status(404).end();
